6c359fedb7561bfae4b64599d20b5684baa818e1
Weeranon1500 <weeranon1500@hotmail.com>
2021-08-04 14:36:07 +0700

    Web Report CSS492

---
+++ public/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdnM7atJJJrVUPLx2OGSomBO4KNw7Zq54",
+  authDomain: "connector-cnt.firebaseapp.com",
+  databaseURL: "https://connector-cnt-default-rtdb.firebaseio.com",
+  projectId: "connector-cnt",
+  storageBucket: "connector-cnt.appspot.com",
+  messagingSenderId: "522964111979",
+  appId: "1:522964111979:web:983eb752ce7ee8358a172e",
+};
+
+firebase.initializeApp(firebaseConfig);
